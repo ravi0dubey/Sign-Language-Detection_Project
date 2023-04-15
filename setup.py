6fa8d8385f8aee1d055feb568d18700e4513421d
Ravi0Dubey <ravi0dubey@gmail.com>
2023-04-15 06:54:02 -0400

    exception

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Ravi Dubey',
+    author_email= 'ravi0dubey@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
